0eef3384d887d769f927d858e2a97ea7f86c4a72
Nicolas Mazzon <nicolasmazzon549@gmail.com>
2021-09-30 14:53:05 +0200

    Cleaned up README generator

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
-# Index
-### Categories
+## business
+[PESTLE](./business/PESTLE.md) <br>
+[five-forces](./business/five-forces.md) <br>
+[boston-matrix](./business/boston-matrix.md) <br>
+[SWOT-Analysis](./business/SWOT-Analysis.md) <br>
+[short-termism](./business/short-termism.md) <br>
+[ansoffs-matrix](./business/ansoffs-matrix.md) <br>
+[porters-matrix](./business/porters-matrix.md) <br>
+[types-of-growth](./business/types-of-growth.md) <br>
+[economies-of-scale](./business/economies-of-scale.md) <br>
+[project-management](./business/project-management.md) <br>
+[corporate-objectives](./business/corporate-objectives.md) <br>
+[political-legal-environment](./business/political-legal-environment.md) <br>
+[problems-arising-from-growth](./business/problems-arising-from-growth.md) <br>
 
-#### [business](./business/README.md)
-
-### Individual
-
-##### [lldb](./lldb.md)
+## Individual
+[lldb](./lldb.md) <br>
--- generate_index.py
+++ generate_index.py
@@ -1,63 +1,69 @@
-from os import listdir, path, getcwd, chdir
+from pathlib import Path
 
-PWD = getcwd()
+PWD = Path('.')
 
-def is_markdown(entry: str) -> bool:
-    if path.isfile(entry):
-        # it's the top level README
-        if entry == "README.md" and getcwd() == PWD:
+def is_markdown(entry: Path) -> bool:
+    if entry.is_file():
+        # if it's the top level README
+        if entry.name == "README.md" and PWD == Path('.'):
             return False
-        if entry[len(entry)-3:] == ".md":
+        if entry.suffix == ".md":
             return True
     return False
 
-def is_dir(entries: list[str]) -> list[str]:
-    return [entry for entry in entries if path.isdir(entry) and entry[0] != "."]
+def is_dir(entries) -> list[Path]:
+    return [entry for entry in entries if entry.is_dir() and entry.name[0] != "."]
 
-def is_file(entries: list[str]) -> list[str]:
-    return [entry for entry in entries if path.isfile(entry) and entry[0] != "."]
+def is_file(entries) -> list[Path]:
+    return [entry for entry in entries if entry.is_file() and entry.name[0] != "."]
 
-def replace_underscore(s: str) -> str:
+def sort_path_len(entries) -> list[Path]:
+    entries = [entry for entry in entries]
+    lengths = [(len(entry.stem), idx) for idx, entry in enumerate(entries)]
+    return [entries[idx] for _, idx in sorted(lengths)]
+
+def path_to_relative(path: Path) -> Path:
+    parts = len(Path(PWD).parts)
+    new_path = Path('')
+    for part in path.parts[parts:]:
+        new_path /= part
+    return new_path
+
+def replace_underscore(path: Path) -> Path:
     sum = ""
-    for char in s:
+    for char in path.name:
         if char == "_":
             sum += "-"
         else:
             sum += char 
-    return sum
+    return path.with_name(sum)
 
 
-def write_markdown_index(dir: str):
+def generate_markdown_subsection(dir: Path) -> str:
     index = ""
 
-    if path.isdir(dir):
-        chdir(path.abspath(dir))
-    else:
+    if not dir.is_dir():
         raise NotADirectoryError
 
-    for entry in listdir():
+    for entry in sort_path_len(dir.iterdir()):
         if is_markdown(entry):
-            name = replace_underscore(entry[:len(entry)-3])
-            index += f"##### [{name}](./{entry})\n"
-    open("README.md", "w").write(index)
-    chdir(PWD)
+            entry = replace_underscore(entry)
+            entry = path_to_relative(entry)
+            index += f"[{entry.stem}](./{entry}) <br>\n"
+    return index
 
 
 def main():
-    open("README.md", 'w').write("# Index\n")
-    handle = open("README.md", 'a')
+    handle = open("README.md", 'w')
+    for dir in is_dir(Path('.').iterdir()):
+        handle.write(f"## {dir.name}\n")
+        handle.write(f"{generate_markdown_subsection(dir)}\n")
 
-    handle.write("### Categories\n")
+    handle.write("## Individual\n")
 
-    for dir in is_dir(listdir()):
-        handle.write(f"\n#### [{dir}](./{dir}/README.md)")
-        write_markdown_index(dir)
-
-    handle.write("\n\n### Individual\n")
-
-    for file in is_file(listdir()):
+    for file in is_file(Path('.').iterdir()):
         if is_markdown(file):
-            handle.write(f"\n##### [{file[:len(file)-3]}](./{file})")
+            handle.write(f"[{file.stem}](./{file}) <br>\n")
 
 if __name__ == "__main__":
     main()
